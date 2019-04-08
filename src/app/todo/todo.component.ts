9da7c6ac0ea599f369308cdd29d9a23fde794ed6
ElMehdi <elmehdi.laghraoui@gmail.com>
2019-04-08 20:45:50 +0200

    socle

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -32,7 +32,7 @@ export class TodoComponent implements OnInit {
 
   //updateTodo
   saveTodo(){
-    if(this.id === -1){
+    if(this.id == -1){
       this.todoService.createTodo('mehdi', this.todo).subscribe(
       data => {
         console.log(data)
